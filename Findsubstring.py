fa58e940fabd5307d2e2ff635da88e0b363c9ecf
anilgolamari <anil.golamari@gmail.com>
2015-10-07 22:47:11 -0400

    Revert "Phython_samplecodes"
    This reverts commit 40fe2fcfa051134d2b57e3d045c443e059de22a9.

--- Findsubstring.py
+++
@@ -1,7 +0,0 @@
-#code base to print repeated word in a string.
-s = 'azcbobobegghakl'
-total = 0
-for i in range(len(s)-2):
-    if s[i:i+3] == 'bob':
-        total += 1
-print 'number of times bob occurs is: ', total
--- Vowelscount.py
+++
@@ -1,8 +0,0 @@
-#Code base to find out number of vowels. 
-# Input string is already provided in this code base. 
-s = 'asldfjasdwetivouasdfs'
-count = 0
-for x in s: 
-    if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u'):        
-        count += 1
-print count
